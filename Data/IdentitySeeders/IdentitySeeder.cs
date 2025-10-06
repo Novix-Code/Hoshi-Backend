74e96c338829444fe9ce1c1205d2170c2ea5d243
abdo <abdelrhman@DESKTOP-G0SHSIH.(none)>
2025-10-06 20:37:37 +0300

    upload Hoshi publish

--- Data/IdentitySeeders/IdentitySeeder.cs
+++ Data/IdentitySeeders/IdentitySeeder.cs
@@ -28,11 +28,6 @@ namespace Hoshi.Data.IdentitySeeders
             var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
 
-            var dictionaryAdminUsers = new Dictionary<string, List<string>>
-            {
-                { "adminEmail" ,new List<string>{"novix@novix.com" , "hoshi@hoshi.com" }} ,
-                { "adminPassword" , new List<string>{"Novix@12345" , "Hoshi@12345"}}
-            };
             // Create Admin role if it doesn't exist
             if (!await roleManager.RoleExistsAsync("SuperAdmin"))
                 await roleManager.CreateAsync(new IdentityRole<int>("SuperAdmin"));
@@ -40,30 +35,49 @@ namespace Hoshi.Data.IdentitySeeders
             if (!await roleManager.RoleExistsAsync(UserType.Admin.ToString()))
                 await roleManager.CreateAsync(new IdentityRole<int>(UserType.Admin.ToString()));
 
-            // Check if admin is exist , if not we will go to create it
-            await CheckThenAddAdminUser(userManager, dictionaryAdminUsers["adminEmail"][0], dictionaryAdminUsers["adminPassword"][0]);
-            await CheckThenAddAdminUser(userManager, dictionaryAdminUsers["adminEmail"][1], dictionaryAdminUsers["adminPassword"][1]);   
+            List<Tuple<User, string>> users = new () 
+            {
+                new(
+                    new()
+                    {
+                        FullName = "نوفكس مشرف",
+                        Email = "info@novixcode.com",
+                        EmailConfirmed = true,
+                        UserCode = "AD-1",
+                        UserName = "NovixCode",
+                        CreatedAt = DateTime.UtcNow,
+                        UserType = UserType.Admin.ToString()
+                    },
+                    "Novix@12345"
+                ),
+                new(
+                    new()
+                    {
+                        FullName = "حوشي مشرف",
+                        Email = "support@hoshi.ly",
+                        EmailConfirmed = true,
+                        UserCode = "AD-2",
+                        UserName = "Hoshi",
+                        CreatedAt = DateTime.UtcNow,
+                        UserType = UserType.Admin.ToString()
+                    },
+                    "Hoshi@12345"
+                )
+            };
+
+            foreach (var user in users)
+                await CheckThenAddAdminUser(userManager, user);
         }
-        public static async Task CheckThenAddAdminUser(UserManager<User> userManager,
-                                                       string adminEmail , string adminPassword)
+        public static async Task CheckThenAddAdminUser(UserManager<User> userManager, Tuple<User, string> user)
         {
             // Check if the admin user exists
-            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            var adminUser = await userManager.FindByEmailAsync(user.Item1.Email!);
             if (adminUser == null)
             {
-                var newAdmin = new User
-                {
-                    UserName = adminEmail,
-                    Email = adminEmail,
-                    EmailConfirmed = true,
-                    CreatedAt = DateTime.UtcNow,
-                    UserType = UserType.Admin.ToString(),
-                };
-                var result = await userManager.CreateAsync(newAdmin, adminPassword);
+                var result = await userManager.CreateAsync(user.Item1, user.Item2);
                 if (result.Succeeded)
                 {
-                    await userManager.AddToRoleAsync(newAdmin, "SuperAdmin");
-                    await userManager.AddToRoleAsync(newAdmin, UserType.Admin.ToString());
+                    await userManager.AddToRolesAsync(user.Item1, ["SuperAdmin", UserType.Admin.ToString()]);
                 }
                 else
                 {
