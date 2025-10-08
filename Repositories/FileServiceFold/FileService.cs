ae70967fc4d5e61d0c4954a12698241a617e6dc5
abdo <abdelrhman@DESKTOP-G0SHSIH.(none)>
2025-10-08 21:31:20 +0300

    upload changes for final upload

--- Repositories/FileServiceFold/FileService.cs
+++ Repositories/FileServiceFold/FileService.cs
@@ -51,6 +51,9 @@ namespace Hoshi.Repositories.FileServiceFold
             if (!ValidateFileExtension(file))
                 return new Tuple<bool, string>(false, FileServiceResults.UnsupportedFileExtension);
 
+            if (!Directory.Exists(_environment.WebRootPath))
+                Directory.CreateDirectory("wwwroot");
+
             //Get Folder Full Path
             string folderFullPath = Path.Combine(_environment.WebRootPath, folderShortPath);
 
